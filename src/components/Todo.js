169a1ec27be6ea804cdbef724ef43fcb92d94992
vinhquang3004 <vinhquang300496@gmail.com>
2021-07-11 02:18:31 +0700

    add checkbox, button, note and style

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,60 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
 
 //test git
 const TodoItem = (props) => {
-    const todo = props.todoProps
+    //style material
+    const useStyles = makeStyles((theme) => ({
+        root: {
+            '& > *': {
+                margin: theme.spacing(1),
+            },
+        },
+    }));
+
+    const todoTitle = props.todoProps.title
+    const todoCompleted = props.todoProps.completed
+    const styleTodo = useStyles();
+    const [checked, setChecked] = React.useState(true);
+    const handleChange = (event) => {
+        setChecked(event.target.checked);
+    };
+
+    //style background totoItem
+    const todoItemStyle = {
+        background: '#f4f4f4',
+        /* padding: '10px', */
+        margin: '15px',
+        borderBottom: '1px #ccc dotted',
+        textDecoration: todoCompleted ? 'none' : 'line-through'
+    }
+
+    //style button delete
+    const buttonDeleteStyle = {
+        float: 'right',
+        borderRadius: '15%'
+    }
+
     return (
-        <p>{todo}</p>
+        <div style={todoItemStyle} className={styleTodo.root}>
+            {/* checkbox */}
+            <Checkbox
+                checked={checked}
+                onChange={handleChange}
+                inputProps={{ 'aria-label': 'primary checkbox' }}
+            />
+            {/* note */}
+            {todoTitle}
+            {/* button delete */}
+            {/* <input type='button' value="delete" /> */}
+            <Button style={buttonDeleteStyle} variant="contained" color="secondary">
+                Delete
+            </Button>
+        </div>
     )
 }
+
 
 export default TodoItem
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,12 +2,16 @@ import React, { useState, Fragment } from 'react'
 import TodoItem from './Todo'
 
 const TodoList = () => {
-    const [todosState, setTodosState] = useState(['todo 1', 'todo 2', 'todo 3'])
+    const [todosState, setTodosState] = useState([
+        { title: 'note 1', completed: false },
+        { title: 'note 2', completed: false },
+        { title: 'note 3', completed: false }
+    ])
 
     return (
         <Fragment>
             {todosState.map(todo => {
-                return <TodoItem todoProps = {todo}/>
+                return <TodoItem todoProps={todo} />
             })}
         </Fragment>
     )
